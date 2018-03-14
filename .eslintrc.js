e351d0c555e9716be54b7db48f97117056d93b91
Dave Donaldson <33964227+Dave-Donaldson@users.noreply.github.com>
2018-03-14 02:21:36 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-ally",
+        "import"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": 0,
+        "no-use-before-define": 0
+    },
+
+};
+
