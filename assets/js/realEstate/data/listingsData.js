9afcf4f25289371e4044b75f8f7282dfdf25d7bc
Dave-DOnaldson <david_donaldson31@yahoo.com>
2018-03-15 15:00:14 -0400

    new features

---
+++ assets/js/realEstate/data/listingsData.js
@@ -0,0 +1,103 @@
+var listingsData = [
+	{
+		address: '401 stansbury way',
+		city: 'Abingdon',
+		state: 'MD',
+		rooms: 2,
+		price: 115000,
+		floorSpace: 950,
+		extras: [
+			'elevator',
+			'gym'
+		],
+		homeType: 'Condo',
+		image: 'https://media.tmz.com/2017/12/28/1228-ellen-degeneres-new-condo-los-angeles-photos-launch-3.jpg'
+	},
+	{
+		address: '900 grand ave',
+		city: 'Abingdon',
+		state: 'VA',
+		rooms: 2,
+		price: 360000,
+		floorSpace: 1700,
+		extras: [
+			'pool',
+			'gym'
+		],
+		homeType: 'Single Family',
+		image: 'http://www.ryanhomes.com/rh-community-gallery-NewAspectRatio/d8b0c394-b123-4d9d-957b-1d24e21d319f/db/d8b0c394-b123-4d9d-957b-1d24e21d319f.jpg'
+	},
+	{
+		address: '906 flower st',
+		city: 'Dover',
+		state: 'DEL',
+		rooms: 2,
+		price: 209000,
+		floorSpace: 1100,
+		extras: [
+			'finished basement',
+			'gym',
+			'pool'
+		],
+		homeType: 'Town house',
+		image: 'https://photos.zillowstatic.com/p_h/ISap17x6qdkgpj1000000000.jpg'
+	},
+	{
+		address: '123 main st',
+		city: 'Spring Grove',
+		state: 'PA',
+		rooms: 3,
+		price: 299000,
+		floorSpace: 1800,
+		extras: [
+			'finished basement',
+			'garage'
+		],
+		homeType: 'single family',
+		image: 'https://photos.zillowstatic.com/p_h/IS6m05bwkj26hc1000000000.jpg'
+	},
+	{
+		address: '1123 lilly dr',
+		city: 'Fallston',
+		state: 'NJ',
+		rooms: 4,
+		price: 400000,
+		floorSpace: 2200,
+		extras: [
+			'garage',
+			'finished basement'
+		],
+		homeType: 'single family',
+		image: 'https://photos.zillowstatic.com/p_h/ISa1j8rkepa2o80000000000.jpg'
+	},
+	{
+		address: '1123 marken dr',
+		city: 'Fallston',
+		state: 'CA',
+		rooms: 5,
+		price: 700000,
+		floorSpace: 2200,
+		extras: [
+			'garage',
+			'finished basement'
+		],
+		homeType: 'single family',
+		image: 'https://photos.zillowstatic.com/p_h/ISukfachunc0u60000000000.jpg'
+	},
+	{
+		address: '112 this dr',
+		city: 'Ridgewood',
+		state: 'NY',
+		rooms: 5,
+		price: 600000,
+		floorSpace: 2200,
+		extras: [
+			'garage',
+			'finished basement'
+		],
+		homeType: 'single family',
+		image: 'https://photos.zillowstatic.com/p_h/IS27yt71yi09ye0000000000.jpg'
+	},
+]
+
+export default listingsData;
--- assets/js/realEstate/realEstate.js
+++ assets/js/realEstate/realEstate.js
@@ -3,22 +3,25 @@ import ReactDOM from 'react-dom'
 import Header from './Header.js'
 import Filter from './Filter.js'
 import Listings from './Listings.js'
+import listingsData from './data/listingsData.js'
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
-      name: 'Joe'
+      name: 'Joe',
+      listingsData
     }
   }
   
   render () {
+    console.log(this.state)
     return (
     <div> 
       <Header /> 
     <section id="content-area">
       <Filter />
-      <Listings />
+      <Listings listingsData={this.state.listingsData} />
     </section>
       </div>
   )
