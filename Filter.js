857f77cc2fb717da952f3de552381d10204e65a7
Dave Donaldson <33964227+Dave-Donaldson@users.noreply.github.com>
2018-03-17 23:07:48 -0400

    Add files via upload

--- Filter.js
+++ Filter.js
@@ -7,8 +7,45 @@ export default class Filter extends Component {
     this.state = {
       name: 'Joe',
     };
+    this.cities = this.cities.bind(this)
+    this.homeTypes = this.homeTypes.bind(this)
+    this.bedrooms = this.bedrooms.bind(this)
   }
-
+  componentWillMount(){
+    this.props.populateAction()
+  }
+  cities() {
+    if(this.props.globalState.populateFormsData.cities != undefined) {
+    var { cities } = this.props.globalState.populateFormsData
+    return cities.map((item) => {
+      return(
+        <option key={item} value={item}>{item}</option>
+        )
+      })
+    }
+  }
+  homeTypes() {
+    if(this.props.globalState.populateFormsData.homeTypes != undefined) {
+      var { homeTypes } = this.props.globalState.populateFormsData
+      return homeTypes.map((item) => {
+        return(
+          <option key={item} value={item}>{item}</option>
+          )
+        })
+      }
+    
+  }
+  bedrooms() {
+    if(this.props.globalState.populateFormsData.bedrooms != undefined) {
+      var { bedrooms } = this.props.globalState.populateFormsData
+      return bedrooms.map((item) => {
+        return(
+          <option key={item} value={item}>{item}+ BR</option>
+          )
+        })
+      }
+    
+  }
   render() {
     return (
       <section id="filter">
@@ -16,58 +53,68 @@ export default class Filter extends Component {
 
           <h4>Filter</h4>
 
-          <select
-          name="neighborhood"
-          className="filters neighborhood"
-        >
-          <option>Abingdon</option>
-        </select>
+          <label htmlFor="city">City</label>
+          <select name="city" className="filters city" onChange={this.props.change}>
+          <option value="All">All</option>
+          {this.cities()}
+          </select>
+          
+          <label htmlFor="homeType">Home Type</label>
+          <select name="homeType" className="filters homeType" onChange={this.props.change}>
+            <option value="All"> All homes  </option>
+            {this.homeTypes()}
+          </select>
 
-          <select name="housetype" className="filters housetype" >
-          <option> Ranch  </option>
-        </select>
-
-          <select name="bedrooms" className="filters bedrooms" >
-          <option > 2 BR </option>
-        </select>
+          <label htmlFor="bedrooms">Bedrooms</label>
+          <select name="bedrooms" className="filters bedrooms"
+           onChange={this.props.change}>
+            <option value="0">0+ BR</option>
+            {this.bedrooms()} + BR
+            
+          </select>
 
           <div className="filters price">
-          <span className="title">Price</span>
-          <input type="text" name="min-price" className="min-price" />
-          <input type="text" name="max-price" className="max-price" />
-        </div>
+            <span className="title">Price</span>
+            <input type="text" name="min_price" className="min-price" onChange={this.props.change} value={this.props.globalState.min_price} />
+            <input type="text" name="max_price" className="max-price" onChange={this.props.change} value={this.props.globalState.max_price} />
+          </div>
 
           <div className="filters floor-space">
-          <span className="title" > Floor Space </span>
-          <input type="text" name="min-floor-space" />
-          <input type="text" name="max-floor-space" />
-        </div>
+            <span className="title" > Floor Space </span>
+            <input type="text" name="min_floor_space" onChange={this.props.change} value={this.props.globalState.min_floor_space} />
+            <input type="text" name="max_floor_space" onChange={this.props.change} value={this.props.globalState.max_floor_space} />
+          </div>
 
           <div className="filters extras">
-          <span className="title">
+            <span className="title">
             Extras
-          </span>
-          <label htmlFor="extras">
-            <span> Elevators </span>
-            <input name="extras" value="elevator" type="checkbox" />
-          </label>
+            </span>
+            <label htmlFor="extras">
+              <span> Elevators </span>
+              <input name="elevator" value="elevator" type="checkbox" />
+            </label>
 
-          <label htmlFor="extras">
-            <span> Swimming Pool </span>
-            <input name="extras" value="swimming-pool" type="checkbox" />
-          </label>
+            <label htmlFor="extras">
+              <span> Swimming Pool </span>
+              <input name="swimming_pool" value="swimming_pool" type="checkbox" onChange={this.props.change} />
+            </label>
 
-          <label htmlFor="extras">
-            <span> Finished Basement </span>
-            <input name="extras" value="finished-basement" type="checkbox" />
-          </label>
+            <label htmlFor="extras">
+              <span> Finished Basement </span>
+              <input name="finished_basement" value="finished-basement" type="checkbox" onChange={this.props.change} />
+            </label>
 
-          <label htmlFor="extras">
-            <span> Gym </span>
-            <input name="extras" value="Gym" type="checkbox" />
-          </label>
+            <label htmlFor="extras">
+              <span> Gym </span>
+              <input name="Gym" value="Gym" type="checkbox" onChange={this.props.change} />
+            </label>
 
-        </div>
+            <label htmlFor="extras">
+              <span> Garage </span>
+              <input name="Garage" value="Garage" type="checkbox" onChange={this.props.change} />
+            </label>
+
+          </div>
         </div>
       </section>
     );
--- Listings.js
+++ Listings.js
@@ -7,12 +7,92 @@ export default class Listings extends Component {
     this.state = {
       name: 'Joe'
     }
+    this.loopListings = this.loopListings.bind(this)
   }
-  
+  loopListings () {
+    var {listingsData} = this.props
+    if(listingsData == undefined || listingsData.length == 0) {
+      return "Sorry your filter did not match any listing "
+    }
+    
+    return listingsData.map( (listing, index) => {
+      return (<div className="col-md-3" key={index}> 
+      <div className="listing" >
+        <div className="listing-img" style={{background: `url("${listing.image}")`}}>
+          <span className="address">{listing.address}</span>
+          <div className="details">
+          <div className="col-md-3">
+          <div className="user-img"></div>
+          </div>
+          <div className="col-md-9">
+            <div className="user-details">
+              <span className="user-name">Nina Smith</span>
+              <span className="post-date">05/05/2017</span>
+            </div>
+            <div className="listing-details">
+              <div className="floor-space"> 
+                <i className="far fa-square"></i><span>{listing.floorSpace} ft&sup2;</span>  
+              </div>
+              <div className="bedrooms">
+                <i className="fas fa-bed"></i><span>{listing.bedrooms} bedrooms</span>
+              </div>
+            </div>
+            <div className="view-btn">View Listing</div>
+          </div>
+        </div>
+      </div>
+        <div className="bottom-info">
+            <span className="price">${listing.price} </span>
+            <span className="location"> <i className="fas fa-map-marker"></i> {listing.city}, {listing.state}</span>
+          </div>
+        </div>
+    </div>)
+    })
+  }
+
   render () {
     return (
-    <section className="listings">
-      Listings    
+    <section id="listings">
+      <div className="container">
+      <section className="search-area">
+      <input type="text" name="search" />
+      </section>
+
+      <div className="sortby-area"> 
+        <div className="results">390 results found</div>
+          <div className="sort-options"> 
+            <select name="sortby" className="sortby">
+              <option value="price-asc">Highest Price</option>
+              <option value = "price-asc" >Lowest Price</option>
+            </select>
+              <div className="view">
+                < i className = "fas fa-th-list" > </i>
+                < i className = "fas fa-th" > </i>
+              </div>
+            </div>
+        
+      </div>
+
+    <section className = "listings-results">
+     
+      {this.loopListings()}
+  
+  </section>
+
+    
+
+      <section id="pagination"> 
+        <ul className="pages">
+          <li>Prev</li>
+          <li>1</li>
+          <li className="active">2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>Next</li>
+        </ul>
+      </section>
+      </div>
     </section>
   )
   }
